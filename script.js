0934dde5cdd387aaf942f95f8fe3fc13edc3bb25
minaemad100 <55882261+minaemad100@users.noreply.github.com>
2019-10-21 21:14:28 +0200

    Update script.js

--- script.js
+++ script.js
@@ -4,9 +4,9 @@ var r3 = document.getElementById('r3')
 
 function calc(){
 
-var ra = (r1 .value * r3.value)/(parseInt(r1.value) + parseInt(r2.value) + parseInt(r3.value));
-var rb = (r1 .value * r2.value)/(parseInt(r1.value) + parseInt(r2.value) + parseInt(r3.value));
-var rc = (r2 .value * r3.value)/(parseInt(r1.value) + parseInt(r2.value) + parseInt(r3.value));
+var ra = (r1 .value * r3.value)/(parseFloat(r1.value) + parseFloat(r2.value) + parseFloat(r3.value));
+var rb = (r1 .value * r2.value)/(parseFloat(r1.value) + parseFloat(r2.value) + parseFloat(r3.value));
+var rc = (r2 .value * r3.value)/(parseFloat(r1.value) + parseFloat(r2.value) + parseFloat(r3.value));
 console.log(ra)
 console.log(rc)
 document.getElementById('ra').innerHTML = ra;
