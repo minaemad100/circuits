47df14f87114cfdc7a52c7da40a553ec1ba2cf88
minaemad100 <55882261+minaemad100@users.noreply.github.com>
2019-10-21 21:13:24 +0200

    Update script2.js

--- script2.js
+++ script2.js
@@ -4,9 +4,9 @@ var r3 = document.getElementById('r3')
 
 function calc(){
 
-var r12 = parseInt(r1.value)+parseInt(r2.value)+parseInt((r1.value*r2.value)/r3.value);
-var r23 = parseInt(r3.value)+parseInt(r2.value)+parseInt((r3.value*r2.value)/r1.value);
-var r31 = parseInt(r1.value)+parseInt(r3.value)+parseInt((r1.value*r3.value)/r2.value);
+var r12 = parseFloat(r1.value)+parseFloat(r2.value)+parseFloat((r1.value*r2.value)/r3.value);
+var r23 = parseFloat(r3.value)+parseFloat(r2.value)+parseFloat((r3.value*r2.value)/r1.value);
+var r31 = parseFloat(r1.value)+parseFloat(r3.value)+parseFloat((r1.value*r3.value)/r2.value);
 
 document.getElementById('r12').innerHTML = r12;
 document.getElementById('r23').innerHTML = r23;
